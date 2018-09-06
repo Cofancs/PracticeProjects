236d5ae1c92595a815eedef9b1fc5fcbb7b1f633
Zsofia Edina Nemeth <zsofia_edina_nemeth@epam.com>
2018-09-06 15:02:47 +0200

    modified sorting input array to Object array and rewrite tests

--- src/main/java/com/cofancs/SortingAlgorithm.java
+++ src/main/java/com/cofancs/SortingAlgorithm.java
@@ -7,44 +7,49 @@ public class SortingAlgorithm {
     public static final String INPUT_ARRAY_SHOULDN_T_BE_NULL = "inputArray shouldn't be null";
     public static final String INPUT_CAN_CONTAINS_DIGITS_AND_A_SINGLE_CHARACTERS = "Input can contains digits and a single \"-\" characters! ";
     public static final String DASH = "-";
+    public static final Character DASH_CHAR = '-';
 
-    public  String[] sortNullsToTheEnd(String[] inputArray){
+    private static boolean testIfContainsInvalidObjects(Object object) {
+        return !((object == null) || (object instanceof Integer) || ((object instanceof Character) && object.equals(DASH_CHAR)) || ((object instanceof String) && object.equals(DASH)));
+    }
+
+    public Object[] sortNullsToTheEnd(Object[] inputArray) throws IllegalArgumentException {
         checkCorrectInput(inputArray);
-        String[] result =doSortingTheEmptyCharacters(inputArray);
+        Object[] result = doSortingTheEmptyCharacters(inputArray);
         return result;
     }
 
-    private String[] doSortingTheEmptyCharacters(String[] inputArray) {
-        for(int i=0;i<inputArray.length-1;i++){
-            if(inputArray[i].equals(DASH)){
-                String element=inputArray[i];
-                for(int j = i+1; j<inputArray.length;j++){
-                    if(!inputArray[j].equals(DASH)){
-                        inputArray[i]=inputArray[j];
-                        inputArray[j]=element;
+    private Object[] doSortingTheEmptyCharacters(Object[] inputArray) {
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == null || inputArray[i].equals(DASH) || inputArray[i].equals(DASH_CHAR)) {
+                inputArray[i] = null;
+                for (int j = i + 1; j < inputArray.length; j++) {
+                    if (!(inputArray[j] == null || inputArray[j].equals(DASH) || inputArray[j].equals(DASH_CHAR))) {
+                        inputArray[i] = inputArray[j];
+                        inputArray[j] = null;
                         break;
                     }
                 }
-
             }
-
         }
         return inputArray;
     }
 
-    private void checkCorrectInput(String[] inputArray) {
+    private void checkCorrectInput(Object[] inputArray) {
         checkIfNull(inputArray);
         checkIfTheElementsHasAnInvalidCharacter(inputArray);
     }
 
-    private void checkIfTheElementsHasAnInvalidCharacter(String[] inputArray) {
-        if(Arrays.stream(inputArray).anyMatch(s -> s.matches("^([0-9]+ | [-])"))){
-            throw new IllegalArgumentException(INPUT_CAN_CONTAINS_DIGITS_AND_A_SINGLE_CHARACTERS);
-        }
+    private void checkIfTheElementsHasAnInvalidCharacter(Object[] inputArray) throws IllegalArgumentException {
+        Arrays.stream(inputArray).filter(
+                SortingAlgorithm::testIfContainsInvalidObjects)
+                .forEach(object -> {
+                    throw new IllegalArgumentException(INPUT_CAN_CONTAINS_DIGITS_AND_A_SINGLE_CHARACTERS);
+                });
     }
 
-    private void checkIfNull(String[] inputArray) {
-        if(inputArray == null){
+    private void checkIfNull(Object[] inputArray) throws IllegalArgumentException {
+        if (inputArray == null || inputArray.length <= 0) {
             throw new IllegalArgumentException(INPUT_ARRAY_SHOULDN_T_BE_NULL);
         }
     }
--- src/test/java/com/cofancs/SortingAlgorithmTest.java
+++ src/test/java/com/cofancs/SortingAlgorithmTest.java
@@ -13,68 +13,74 @@ public class SortingAlgorithmTest {
     @Test
     @DisplayName("╯°□°）╯")
     public void sortNullsToTheEndShouldReturnCorrectOrder(){
-        String[] inputArray= {"4","-","8","-","-","3"};
-        String[] result=underTest.sortNullsToTheEnd(inputArray);
-        String[] expected = {"4","8","3","-","-","-",};
+        Object[] inputArray= {4,"-",8,"-","-",3};
+        Object[] result=underTest.sortNullsToTheEnd(inputArray);
+        Object[] expected = {4,8,3,null,null,null};
         assertArrayEquals(expected,result);
     }
 
     @Test
     @DisplayName("╯°□°）╯already in correct order")
     public void sortNullsToTheEndThatIsAlreadyInCorrectOrder(){
-        String[] inputArray= {"4","8","3","-","-","-",};
-        String[] result=underTest.sortNullsToTheEnd(inputArray);
-        String[] expected = {"4","8","3","-","-","-",};
+        Object[] inputArray= {4,8,3,"-","-","-"};
+        Object[] result=underTest.sortNullsToTheEnd(inputArray);
+        Object[] expected = {4,8,3,null,null,null};
         assertArrayEquals(expected,result);
     }
 
     @Test
     @DisplayName("╯°□°）╯2")
     public void sortNullsToTheEndShouldReturnCorrectOrderForAnotherInput(){
-        String[] inputArray= {"4","5","-","-","-","8","-","-","3"};
-        String[] result=underTest.sortNullsToTheEnd(inputArray);
-        String[] expected = {"4","5","8","3","-","-","-","-","-"};
+        Object[] inputArray= {4,5,"-","-","-",8,"-","-",3};
+        Object[] result=underTest.sortNullsToTheEnd(inputArray);
+        Object[] expected = {4,5,8,3,null,null,null,null,null};
         assertArrayEquals(expected,result);
     }
 
     @Test
     @DisplayName("╯°□°）╯-----")
     public void sortNullsToTheEndShouldReturnCorrectOrderForInputWhereAreOnlyEmptyElements(){
-        String[] inputArray= {"-","-","-","-","-","-","-","-","-"};
-        String[] result=underTest.sortNullsToTheEnd(inputArray);
-        String[] expected = {"-","-","-","-","-","-","-","-","-"};
+        Object[] inputArray= {"-",'-',"-","-","-","-","-","-","-"};
+        Object[] result=underTest.sortNullsToTheEnd(inputArray);
+        Object[] expected = {null,null,null,null,null,null,null,null,null};
         assertArrayEquals(expected,result);
     }
 
     @Test
     @DisplayName("╯°□°）╯12324534")
     public void sortNullsToTheEndShouldReturnCorrectOrderForInputWhereAreOnlyNumbersElements(){
-        String[] inputArray= {"1","2","3","5","3","6","8","4","2"};
-        String[] result=underTest.sortNullsToTheEnd(inputArray);
-        String[] expected = {"1","2","3","5","3","6","8","4","2"};
+        Object[] inputArray= {1,2,4,8,9,6,2,5,4,7,8,5};
+        Object[] result=underTest.sortNullsToTheEnd(inputArray);
+        Object[] expected = {1,2,4,8,9,6,2,5,4,7,8,5};
         assertArrayEquals(expected,result);
     }
 
     @Test
-    @DisplayName("╯°□°）╯'NULL'")
-    public void sortNullsToTheEndThrowsExceptionForNull(){
-        String[] inputArray= {};
-        underTest.sortNullsToTheEnd(inputArray);
-        assertThrows(IllegalArgumentException.class,()->{throw new IllegalArgumentException(SortingAlgorithm.INPUT_ARRAY_SHOULDN_T_BE_NULL);});
+    @DisplayName("╯°□°）╯'EMPTY'")
+    public void sortNullsToTheEndThrowsExceptionForEmptyArray(){
+        Object[] inputArray= {};
+        assertThrows(IllegalArgumentException.class,()->{underTest.sortNullsToTheEnd(inputArray);},SortingAlgorithm.INPUT_ARRAY_SHOULDN_T_BE_NULL);
     }
     @Test
     @DisplayName("╯°□°）╯'abc'")
     public void sortNullsToTheEndThrowsExceptionForNotCorrectInputABC(){
-        String[] inputArray= {"4","-","bf"};
-        underTest.sortNullsToTheEnd(inputArray);
-        assertThrows(IllegalArgumentException.class,()->{throw new IllegalArgumentException(SortingAlgorithm.INPUT_CAN_CONTAINS_DIGITS_AND_A_SINGLE_CHARACTERS);});
+        Object[] inputArray= {"-",'-',4,"bf"};
+        assertThrows(IllegalArgumentException.class,()->{underTest.sortNullsToTheEnd(inputArray);},SortingAlgorithm.INPUT_CAN_CONTAINS_DIGITS_AND_A_SINGLE_CHARACTERS);
     }
 
     @Test
     @DisplayName("╯°□°）╯'abc2'")
     public void sortNullsToTheEndThrowsExceptionForNotCorrectInputDoubleDash(){
-        String[] inputArray= {"43","4","--","-"};
-        underTest.sortNullsToTheEnd(inputArray);
-        assertThrows(IllegalArgumentException.class,()->{throw new IllegalArgumentException(SortingAlgorithm.INPUT_CAN_CONTAINS_DIGITS_AND_A_SINGLE_CHARACTERS);});
+        Object[] inputArray= {43,4,"--","-"};
+        assertThrows(IllegalArgumentException.class,()->{underTest.sortNullsToTheEnd(inputArray);},SortingAlgorithm.INPUT_CAN_CONTAINS_DIGITS_AND_A_SINGLE_CHARACTERS);
+    }
+
+    @Test
+    @DisplayName("╯°□°）╯everything")
+    public void sortNullsToTheEndThrowsExceptionForCorrectInputWithNullsAndDashesAndNumbers(){
+        Object[] inputArray= {1,null,4,'-',9,"-",2,'-',4,null,8,5};
+        Object[] result=underTest.sortNullsToTheEnd(inputArray);
+        Object[] expected = {1,4,9,2,4,8,5,null,null,null,null,null};
+        assertArrayEquals(expected,result);
     }
 }
